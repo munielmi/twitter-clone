7064ecb6013c240380deac6e7aa6d5297976d815
munielmi123 <munielmi123@gmail.com>
2022-04-22 12:03:45 -0700

    added files

---
+++ static/js/posts.js
@@ -0,0 +1,9 @@
+//////////////////////////
+//Javascript for post page
+//////////////////////////
+
+$(function(){
+    $('.js-menu-icon').click(function(){
+        $(this).next().toggle();
+    })
+})
